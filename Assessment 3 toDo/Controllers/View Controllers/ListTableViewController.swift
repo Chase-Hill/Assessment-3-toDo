7e08d6377895ebe6a6434f81867112ec8eb1e3da
Chase <chaseaohill@gmail.com>
2023-02-15 19:30:31 -0500

    Added remaining outlets to view controllers and updated functions within them

--- Assessment 3 toDo/Controllers/View Controllers/ListTableViewController.swift
+++ Assessment 3 toDo/Controllers/View Controllers/ListTableViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    // MARK: - Outlet
+    
+    @IBOutlet weak var listNameTextField: UITextField!
+    
     // MARK: - LifeCycle
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,55 +34,34 @@ class ListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
-
-        // Configure the cell...
+        let listDisplayed = ListController.shared.lists[indexPath.row]
+        var cellConfig = cell.defaultContentConfiguration()
+        cellConfig.text = listDisplayed.title
+        cellConfig.secondaryText = "\(listDisplayed.tasks.count)"
+        cell.contentConfiguration = cellConfig
 
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let listToBeDeleted = ListController.shared.lists[indexPath.row]
+            ListController.shared.deleteList(listToDelete: listToBeDeleted)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "toTask" {
+            if let indexPath = tableView.indexPathForSelectedRow,
+               let destinationVC = segue.destination as? TaskTableViewController {
+                let listToSend = ListController.shared.lists[indexPath.row]
+                destinationVC.listReciever = listToSend
+            }
+        }
     }
-    */
-
 }
--- Assessment 3 toDo/Controllers/View Controllers/TaskTableViewController.swift
+++ Assessment 3 toDo/Controllers/View Controllers/TaskTableViewController.swift
@@ -2,14 +2,16 @@ import UIKit
 
 class TaskTableViewController: UITableViewController {
 
+    var listReciever: List?
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
